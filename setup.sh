61c5ce7658b8725d80816ba0c88ee6582931d018
Markus van Kempen <markusvankempen43@gmail.com>
2017-11-13 11:31:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ mkdir /home/pi/gitupdater
 cd  /home/pi/gitupdater/
 git  clone https://github.com/markusvankempen/snpcode.git
 cd  /home/pi/gitupdater/snpcode/
-sudo cp  gitupdatersnpbak.service /etc/systemd/system/
+sudo cp  gitupdatersnp.service /etc/systemd/system/
 sudo systemctl enable gitupdatersnp.service
 sudo systemctl start gitupdatersnp.service
 sudo systemctl status gitupdatersnp.service
