ec49fbcd6f3095ff05dddaafd54e489eed54409f
Markus van Kempen <markusvankempen43@gmail.com>
2017-11-12 20:51:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-echo "git Setup script"
+
+echo "Setup snpcode for git sync"
+mkdir /home/pi/gitupdater
+cd  /home/pi/gitupdater/
+git  clone https://github.com/markusvankempen/snpcode.git
+cd  /home/pi/gitupdater/snpcode/
+sudo cp  gitupdatersnpbak.service /etc/systemd/system/
+sudo systemctl enable gitupdatersnp.service
+sudo systemctl start gitupdatersnp.service
+sudo systemctl status gitupdatersnp.service
+
