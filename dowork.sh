04b045c9b744f77fe71864f2893344c715da7653
Markus van Kempen <markusvankempen43@gmail.com>
2018-02-05 13:05:57 -0500

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -1,4 +1,21 @@
+#
 echo "The working script ! mvk-20180205"
 date
 echo "Copy pa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf"
 sudo cp /home/pi/gitupdater/snpcode/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+echo "Updateing script";
+echp "Backup old script pipbiotv2.js ");
+sudo mv /home/pi/playbulb/snp00/pipbiotv2.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-2018020
+sudo rm /home/pi/playbulb/snp00/pipbiotv2.js
+echo "cp pipbgpiov1.js  to pipbiotv2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbiotv2.js
+echo "cp pipbgpiov1.js  to pipbgpiov1-is-in-pipbiotv2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbgpiov1-is-in-pipbiotv2.js             
+echo "cp pipbgpiov1.js  to snp00/ pipbgpiov1.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbgpiov1.js
+echo "cp andle-service.js /home/pi/playbulb/lib/"          
+sudo cp /home/pi/gitupdater/snpcode/candle-service.js /home/pi/playbulb/lib/
+sudo npm install pigpio --prefix /home/pi/playbulb/snp00/
+sleep 30
+#sudo reboot
