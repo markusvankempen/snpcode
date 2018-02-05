2c4f0649e498daa7ce6925a813c8ab23c3023986
Markus van Kempen <markusvankempen43@gmail.com>
2018-02-05 13:41:54 -0500

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -1,6 +1,6 @@
 #
 echo "The working script ! mvk-20180205"
-date > dowork.log
+date > dowork1.log
 echo "Copy pa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf"
 sudo cp /home/pi/gitupdater/snpcode/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
