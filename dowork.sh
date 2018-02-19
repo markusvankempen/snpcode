befae37ade9e1c0c9f6b8036dc0cd631a9f48d6b
Markus van Kempen <markusvankempen43@gmail.com>
2018-02-19 18:06:58 -0500

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -6,7 +6,7 @@ sudo cp /home/pi/gitupdater/snpcode/wpa_supplicant.conf /etc/wpa_supplicant/wpa_
 
 echo "Updateing script";
 echo "Backup old script pipbiotv2.js "
-sudo mv /home/pi/playbulb/snp00/pipbiotv2.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-2018020
+sudo mv /home/pi/playbulb/snp00/pipbiotv2.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-20180219
 echo "cp pipbgpiov1.js  to pipbiotv2.js"
 sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbiotv2.js
 echo "cp pipbgpiov2.js  to pipbgpiov2-is-in-pipbiotv2.js"
@@ -17,7 +17,7 @@ echo "cp 7segmentv3.js  to snp00/ 7segmentv3.js"
 sudo cp /home/pi/gitupdater/snpcode/7segmentv3.js /home/pi/playbulb/snp00/7segmentv3.js
 echo "cp andle-service.js /home/pi/playbulb/lib/"          
 sudo cp /home/pi/gitupdater/snpcode/candle-service.js /home/pi/playbulb/lib/
-sudo npm install pigpio --prefix /home/pi/playbulb/snp00/
+//sudo npm install pigpio --prefix /home/pi/playbulb/snp00/
 echo "Reboot in 60sec"
-sleep 60
+sleep 30
 sudo reboot
