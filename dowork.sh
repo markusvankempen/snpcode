bd473f9d7f44c74dfa225f73f6373aa2937604e6
Markus van Kempen <markusvankempen43@gmail.com>
2018-02-19 17:58:44 -0500

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -7,13 +7,14 @@ sudo cp /home/pi/gitupdater/snpcode/wpa_supplicant.conf /etc/wpa_supplicant/wpa_
 echo "Updateing script";
 echo "Backup old script pipbiotv2.js "
 sudo mv /home/pi/playbulb/snp00/pipbiotv2.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-2018020
-sudo rm /home/pi/playbulb/snp00/pipbiotv2.js
 echo "cp pipbgpiov1.js  to pipbiotv2.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbiotv2.js
-echo "cp pipbgpiov1.js  to pipbgpiov1-is-in-pipbiotv2.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbgpiov1-is-in-pipbiotv2.js             
-echo "cp pipbgpiov1.js  to snp00/ pipbgpiov1.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov1.js /home/pi/playbulb/snp00/pipbgpiov1.js
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbiotv2.js
+echo "cp pipbgpiov2.js  to pipbgpiov2-is-in-pipbiotv2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbgpiov2-is-in-pipbiotv2.js             
+echo "cp pipbgpiov2.js  to snp00/ pipbgpiov2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbgpiov2.js
+echo "cp 7segmentv3.js  to snp00/ 7segmentv3.js"
+sudo cp /home/pi/gitupdater/snpcode/7segmentv3.js /home/pi/playbulb/snp00/7segmentv3.js
 echo "cp andle-service.js /home/pi/playbulb/lib/"          
 sudo cp /home/pi/gitupdater/snpcode/candle-service.js /home/pi/playbulb/lib/
 sudo npm install pigpio --prefix /home/pi/playbulb/snp00/
