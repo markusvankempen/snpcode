efeadc231234619004d6a15be965e3ca66be5048
Markus van Kempen <markusvankempen43@gmail.com>
2018-03-01 12:55:40 -0500

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -1,18 +1,18 @@
 #
-echo "The working script ! mvk-201802120-0900"
+echo "The working script ! mvk-201800301-0900"
 date > dowork1.log
 echo "Copy pa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf"
 sudo cp /home/pi/gitupdater/snpcode/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "Updateing script";
 echo "Backup old script pipbiotv2.js "
-sudo mv /home/pi/playbulb/snp00/pipbiotv2.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-20180219
-echo "cp pipbgpiov2.js  to pipbiotv2.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbiotv2.js
-echo "cp pipbgpiov2.js  to pipbgpiov2-is-in-pipbiotv2.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbgpiov2-is-in-pipbiotv2.js             
-echo "cp pipbgpiov2.js  to snp00/ pipbgpiov2.js"
-sudo cp /home/pi/gitupdater/snpcode/pipbgpiov2.js /home/pi/playbulb/snp00/pipbgpiov2.js
+sudo mv /home/pi/playbulb/snp00/pipbiotv3.js /home/pi/playbulb/snp00/pipbiotv2.old.mvk-20180301
+echo "cp pipbgpiov3.js  to pipbiotv2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov3.js /home/pi/playbulb/snp00/pipbiotv2.js
+echo "cp pipbgpiov3.js  to pipbgpiov3-is-in-pipbiotv2.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov3.js /home/pi/playbulb/snp00/pipbgpiov3-is-in-pipbiotv2.js             
+echo "cp pipbgpiov3.js  to snp00/ pipbgpiov3.js"
+sudo cp /home/pi/gitupdater/snpcode/pipbgpiov3.js /home/pi/playbulb/snp00/pipbgpiov3.js
 echo "cp 7segmentv3.js  to snp00/ 7segmentv3.js"
 sudo cp /home/pi/gitupdater/snpcode/7segmentv3.js /home/pi/playbulb/snp00/7segmentv3.js
 echo "cp andle-service.js /home/pi/playbulb/lib/"          
