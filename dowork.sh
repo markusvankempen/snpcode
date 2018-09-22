1e5207be1db175f29ba28e27091773331f363d93
Markus van Kempen <markusvankempen43@gmail.com>
2018-09-22 17:39:22 -0400

    Update dowork.sh

--- dowork.sh
+++ dowork.sh
@@ -15,9 +15,17 @@ echo "cp pipbgpiov3.js  to snp00/ pipbgpiov3.js"
 sudo cp /home/pi/gitupdater/snpcode/pipbgpiov3.js /home/pi/playbulb/snp00/pipbgpiov3.js
 echo "cp 7segmentv3.js  to snp00/ 7segmentv3.js"
 sudo cp /home/pi/gitupdater/snpcode/7segmentv3.js /home/pi/playbulb/snp00/7segmentv3.js
+echo "cp photo.js  to snp00/ photo.js"
+sudo cp /home/pi/gitupdater/snpcode/photo.js /home/pi/playbulb/snp00/photo.js
 echo "cp candle-service.js /home/pi/playbulb/lib/"          
 sudo cp /home/pi/gitupdater/snpcode/candle-service.js /home/pi/playbulb/lib/
 #npm install pigpio --prefix /home/pi/playbulb/snp00/
+echo "Install PICam"
+cd /home/pi/playbulb/snp00/
+git clone https://github.com/troyth/node-raspicam.git
+cd node-raspicam
+npm install 
+
 echo "Install EV3"
 mkdir /home/pi/EV3
 cd /home/pi/EV3
